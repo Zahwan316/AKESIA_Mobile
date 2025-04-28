9f0cfcf04f62b3a05d4aeafa8045e6aade15974c
zahwan316 <thewanes316@gmail.com>
2025-04-28 23:44:09 +0700

    [add]: added home page

--- src/route/Index.tsx
+++ src/route/Index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import LandingScreen from '../screen/Landing';
 import LoginScreen from '../screen/Login';
 import CompleteProfileOrangTuaScreen from '../screen/CompleteProfileOrangTua';
@@ -10,14 +10,50 @@ import HomeScreen from '../screen/Home';
 const tab = createBottomTabNavigator();
 
 const RouteNavigation = (): React.JSX.Element => {
-  return(
+  return (
     <NavigationContainer>
-      <tab.Navigator initialRouteName="ProfileBidan">
-        <tab.Screen name="Landing" component={LandingScreen} options={{headerShown: false, tabBarStyle: {display: 'none'}}} />
-        <tab.Screen name="Login" component={LoginScreen} options={{headerShown: false, tabBarStyle: {display: 'none'}}} />
-        <tab.Screen name="ProfileOrangTua" component={CompleteProfileOrangTuaScreen} options={{headerShown: false, tabBarStyle: {display: 'none'}}} />
-        <tab.Screen name="ProfileBidan" component={CompleteProfileBidanScreen} options={{headerShown: false, tabBarStyle: {display: 'none'}}} />
-        <tab.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+      <tab.Navigator initialRouteName="Home">
+        <tab.Screen
+          name="Landing"
+          component={LandingScreen}
+          options={{
+            headerShown: false,
+            tabBarButton: () => null,
+            tabBarStyle: {display: 'none'},
+          }}
+        />
+        <tab.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+            tabBarButton: () => null,
+            tabBarStyle: {display: 'none'},
+          }}
+        />
+        <tab.Screen
+          name="ProfileOrangTua"
+          component={CompleteProfileOrangTuaScreen}
+          options={{
+            headerShown: false,
+            tabBarButton: () => null,
+            tabBarStyle: {display: 'none'},
+          }}
+        />
+        <tab.Screen
+          name="ProfileBidan"
+          component={CompleteProfileBidanScreen}
+          options={{
+            headerShown: false,
+            tabBarStyle: {display: 'none'},
+            tabBarButton: () => null,
+          }}
+        />
+        <tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
       </tab.Navigator>
     </NavigationContainer>
   );
--- src/section/home/index.tsx
+++ src/section/home/index.tsx
@@ -1,15 +1,191 @@
-import { JSX } from "react";
-import { Text } from "react-native";
-import { SafeAreaView, View } from "react-native";
+import { JSX } from 'react';
+import { Image, ImageSourcePropType, ScrollView, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
+import { heightPercentageToDP, widthPercentageToDP } from 'react-native-responsive-screen';
+import { BUTTON_COLOR, BUTTON_COLOR_2, SECONDARY_COLOR, THIRD_COLOR } from '../../constants/color';
+import ICON from '../../component/icon';
+
+type menu = {
+  name: string,
+  icon: ImageSourcePropType,
+  onChange?: () => void
+}
+
+const menuList: menu[] = [
+  {
+    name: 'Buat Janji',
+    icon: require('../../assets/icon/buat_janji.png'),
+  },
+  {
+    name: 'Pemeriksaan',
+    icon: require('../../assets/icon/pemeriksaan.png'),
+  },
+];
 
 const HomeSection = (): JSX.Element => {
   return(
     <SafeAreaView>
-      <View>
-        <Text>Hi, Lorem ipsum dolor</Text>
-      </View>
+      <ScrollView>
+        <View style={Style.profileContainer}>
+          <View style={Style.nameContainer}>
+            <Text style={{fontSize: 18, color: "#fff"}}>Hi, Lorem ipsum dolor</Text>
+            <Image
+              source={require('../../assets/icon/bell.png')}
+              style={{width: 28, height: 28}}
+              resizeMode='contain'
+            />
+          </View>
+          <View style={Style.boxGroupContainer}>
+            <TouchableOpacity style={Style.boxContainer}>
+              <View style={{width: '30%', height: '100%', marginRight: 8, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <Image
+                  source={require('../../assets/icon/baby.png')}
+                  style={{width: "80%", height: "80%"}}
+                />
+              </View>
+              <View> 
+                <Text style={{fontSize: 14, color: '#fff', fontWeight: 'bold', marginBottom: 4}}>Babyku</Text>
+                <Text style={{fontSize: 12, color: '#fff', fontWeight: 'bold'}}>7 Minggu</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={[Style.boxContainer, {justifyContent: 'center', alignItems: 'center'}]}>
+              <Text style={{fontSize: 14, color: '#fff', fontWeight: 'bold',}}>Tambah Anak</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={Style.menuMainContainer}>
+          <View style={Style.menuHeaderContainer}>
+            <Text style={{fontSize: 16}}>Fitur Rekomendasi</Text>
+          </View>
+          <View style={Style.menuContainer}>
+            {
+              menuList.map((item, index) => (
+                <TouchableOpacity style={Style.menuItemContainer} key={index}>
+                  <View style={{width: "100%", height: "50%", display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: 4}}>
+                    <Image
+                      source={item.icon}
+                      style={{width: "100%", height: "100%"}}
+                      resizeMethod="auto"
+                      resizeMode="contain"
+                    />
+                  </View>
+                  <Text style={{fontSize: 13}}>{item.name}</Text>
+                </TouchableOpacity>
+              ))
+            }
+          </View>
+        </View>
+        <View style={Style.bannerContainer}>
+          <View style={Style.ctaContainer}>
+            <Text>Hubungi Kami</Text>
+            <Image
+              source={require("../../assets/icon/whatsappicon.png")}
+            />
+          </View>
+          <View style={Style.bannerItemContainer}>
+            <View>
+              <Image
+                source={require('../../assets/img/banner.png')}
+              />
+            </View>
+              <Image
+                source={require('../../assets/img/banner.png')}
+              />
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
+
+const Style = StyleSheet.create({
+  profileContainer: {
+    width: widthPercentageToDP(100),
+    height: heightPercentageToDP(25),
+    backgroundColor: THIRD_COLOR,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    padding: 16,
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  menuMainContainer: {
+    width: widthPercentageToDP(100),
+    height: heightPercentageToDP(40),
+    padding: 16,
+    borderWidth: 0,
+  },
+  menuHeaderContainer: {
+    width: '100%',
+    height: '10%',
+    marginBottom: 16,
+  },
+  menuContainer: {
+    width: '100%',
+    height: '80%',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 8,
+    borderWidth: 0,
+  },
+  menuItemContainer: {
+    width: '25%',
+    height: '40%',
+    borderWidth: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bannerContainer: {
+    width: widthPercentageToDP(100),
+    height: heightPercentageToDP(30),
+    borderWidth: 0,
+    padding: 15,
+    marginBottom: 16,
+  },
+  nameContainer: {
+    width: '100%',
+    height: '20%',
+    marginBottom: 32,
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  boxGroupContainer: {
+    width: '100%',
+    height: '50%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  boxContainer: {
+    width: '45%',
+    height: '70%',
+    backgroundColor: SECONDARY_COLOR,
+    borderRadius: 8,
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 8,
+  },
+  ctaContainer: {
+    width: '100%',
+    height: '10%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 16,
+    flexDirection: 'row',
+    gap: 8,
+  },
+  bannerItemContainer: {
+    width: '100%',
+    height: '90%',
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 12,
+  },
+});
 
 export default HomeSection;
